ff1617f96b901727cb31f95e458c3159836996e3
Jorgito <jorgelhd94@gmail.com>
2021-12-12 11:37:46 -0500

    Styling QR

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,89 +1,15 @@
 <template>
   <div class="container 2xl:w-1/2 rounded-lg bg-white lg:mt-36 mt-6 px-10 pt-5">
-    <logo />
+    <Logo />
 
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <div class="flex flex-nowrap flex-col lg:flex-row">
+      <div class="flex-1 bg-red-600 order-2 lg:order-1 px-6 py-6">
+        <p>Control Panel</p>
+      </div>
+      <div class="flex-1 order-1 lg:order-2 px-6 py-6">
+        <QR />
+      </div>
+    </div>
 
     <Footer />
   </div>
@@ -92,6 +18,7 @@
 <script>
 import Logo from './Logo.vue';
 import Footer from './Footer.vue';
+import QR from './QR/QR.vue';
 
 export default {
   name: 'HelloWorld',
@@ -101,6 +28,7 @@ export default {
   components: {
     Logo,
     Footer,
+    QR,
   },
 };
 </script>
---
+++ src/components/QR/QR.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="qr-code" ref="qrCode"></div>
+</template>
+
+<script>
+import QRCodeStyling from 'qr-code-styling';
+
+export default {
+  name: 'QR',
+  mounted: function () {
+    this.qrCode.append(this.$refs['qrCode']);
+    document.getElementsByTagName('canvas')[0].classList.add('mx-auto');
+  },
+  watch: {
+    ['options.data']: function () {
+      this.qrCode.update(this.options);
+    },
+  },
+  data() {
+    return {
+      qrCode: new QRCodeStyling({
+        data: 'http://qr-code-styling.com',
+      }),
+    };
+  },
+};
+</script>
