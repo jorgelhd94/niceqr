a777320296733d33c77abf1188af65c78ec8d293
Jorgito <jorgelhd94@gmail.com>
2021-12-12 10:29:36 -0500

    Create Logo and Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-center p-6">
+    <h3 class="text-xl">Copyright 2022 - Created By JCode Studio</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
---
+++ src/components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center mb-8">
+    <img
+      class="mx-auto"
+      src="@/assets/img/logo.gif"
+      width="250"
+      alt=""
+      srcset=""
+    />
+    <h2 class="text-2xl">A beautiful QR Code generator</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+};
+</script>
